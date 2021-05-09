608c5eef2225940b8394e30e884b469c01a46651
Suriya <suriya.mee29@gmail.com>
2021-05-09 20:58:57 +0700

    2.2.0

---
+++ WatchYourMind/Model/ActivitiesIconModel.swift
@@ -0,0 +1,12 @@
+//
+//  ActivitiesModel.swift
+//  WatchYourMind
+//
+//  Created by Suriya on 8/5/2564 BE.
+//
+
+import Foundation
+struct ActivitiesIcon: Identifiable,Codable {
+    let id : String
+    let iconName : String
+}
